6aad612f2b8a9c19c8e28eef65b8fdb81ce91396
Krishnendra Tiwari <krishnendratiwari09@gmail.com>
2025-08-02 12:15:49 +0530

    i was added the modified task

---
+++ task15.js
@@ -0,0 +1,20 @@
+const number = 10900;
+
+if (number % 3 === 0) {
+    console.log(`${number} is a multiple of 3\n`);
+}
+else {
+    console.log(`${number} is not a multiple of 3\n`);
+}
+
+const sentence = "this is javascript task";
+
+const  word ="is";
+
+if(sentence.includes(word)){
+    console.log(word,"exists in the sentence")
+}
+else{
+    console.log(word,"does not exist in the sentence")
+}
+
