44de20f0a147f0fd9fd5b51fa71a92c0847bd8af
Krishnendra Tiwari <krishnendratiwari09@gmail.com>
2025-09-07 21:19:58 +0530

    new file

---
+++ task16,3.js
@@ -0,0 +1,14 @@
+let counteven = 0;
+let countodd = 0;
+
+for(let i=1; i<=999; i++){
+    if(i%2==0){
+        counteven++;
+    }
+    else{
+        countodd++;
+    }
+
+}
+console.log("even:-",counteven,"odd:-",countodd)
+
